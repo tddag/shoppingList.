455397b40a471617f2c0702589b151c695ebe5f8
tddag <34137087+tddag@users.noreply.github.com>
2019-02-25 13:12:32 -0500

    Update AppNavbar.js

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://github.com">
+                                    <NavLink href="https://github.com/tddag">
                                         Github
                                     </NavLink>
                                 </NavItem>
